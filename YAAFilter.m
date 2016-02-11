f3d11da39ceb469ceb47851037af891af60d9542
Lucas Denys <lmd238@cornell.edu>
2016-02-11 17:42:51 -0500

    averaging over avgIndex

--- YAAFilter.m
+++ YAAFilter.m
@@ -22,7 +22,7 @@
     self.originalPix = [[pixList objectAtIndex: curSlice] copy];
     [NSBundle loadNibNamed:@"Slider" owner:self];
     [NSApp beginSheet: window modalForWindow:[NSApp keyWindow] modalDelegate:self didEndSelector:nil contextInfo:nil];
-    self.slider.maxValue = [pixList count];
+    self.slider.maxValue = 32;
     
     return 0;
 }
@@ -38,68 +38,36 @@
 {
     
     ViewerController *new2DViewer = [self duplicateCurrent2DViewerWindow];
-    float *fImage; // Grey Image
-    int compFactor = self.slider.intValue; // Compression factor
+    int compFactor = self.slider.intValue; // Compression factor: the amount of averages that need to be calculated.
     
     NSArray *pixList = [new2DViewer pixList: 0];
-    int amountOfImages = [pixList count];
-    int imagesPerSample = amountOfImages / compFactor;
     int curSlice = [[new2DViewer imageView] curImage];
     DCMPix *curPix = [pixList objectAtIndex: curSlice];
+    float *fImage = [curPix fImage];
+    int curPos;
+    int pixelsInAvg = [curPix pheight] / compFactor;
     
-    // Number of Pixels
-    int curPos = [curPix pheight] * [curPix pwidth];
-    
-    // Display a waiting window
-    // id waitWindow = [viewerController startWaitWindow:@"Working..."];
-    
-    
-    fImage = [curPix fImage];
-    // Initiate a NSArray with the neighboring DCMPix
-    NSArray *neighbors = [NSArray array];
-    for(int index = MAX(0,curSlice-imagesPerSample/2); index < MIN(curSlice + imagesPerSample/2 - 1, amountOfImages); index++)
-    {
-        //[viewerController setImageIndex: index];
-        neighbors = [neighbors arrayByAddingObject:[pixList objectAtIndex: index]];
-    }
-    //[viewerController setImageIndex:curSlice];
-    while ( curPos--> 0 )
-    {
-        float GreyValue = fImage[curPos];
-        // Iterate over Neighboring DCMPix to calculate the avg for each position
-        for (int i = 0; i < [neighbors count]; i++)
-        {
-            DCMPix *current_Pix = [neighbors objectAtIndex:i];
-            float *current_fImage = [current_Pix fImage];
-            GreyValue += current_fImage[curPos];
+    for (int width = 0; width < [curPix pwidth]; width ++) {
+         for (int avgIndex = 0; avgIndex < compFactor; avgIndex ++) {
+             float avg = 0;
+             int partHeight = MIN(pixelsInAvg,[curPix pheight] - avgIndex * pixelsInAvg);
+             for (int i = 0; i < partHeight; i++) {
+                 curPos = width + [curPix pwidth] * (avgIndex * pixelsInAvg + i);
+                 avg += fImage[curPos];
+             }
+             avg = avg / partHeight;
+             for (int i = 0; i < partHeight; i++) {
+                 curPos = width + [curPix pwidth] * (avgIndex * pixelsInAvg + i);
+                 fImage[curPos] = avg;
+             }
+             //curPos = height * [curPix pwidth] + width;
+             
         }
-        
-        // Writing Pixel
-        fImage[curPos] = GreyValue/imagesPerSample;
     }
-    
-    // if you've changed pixel values,
-    // don't forget to update the display
     
     [new2DViewer needsDisplayUpdate];
     
 }
-
-//- (void) restoreDCM: (DCMPix*)originalDCM
-//{
-//    NSArray *pixList = [viewerController pixList: 0];
-//    int curSlice = [[viewerController imageView] curImage];
-//    DCMPix *curPix = [pixList objectAtIndex: curSlice];
-//    int curPos = [curPix pheight] * [curPix pwidth];
-//    float *fImage = [curPix fImage];
-//    float *originalImage = [originalDCM fImage];
-//    
-//    while ( curPos--> 0 )
-//    {
-//        fImage[curPos] = originalImage[curPos];
-//    }
-//    [viewerController needsDisplayUpdate];
-//}
 
 - (IBAction) closeDialog:(id)sender
 {
@@ -108,8 +76,6 @@
     
     if( [sender tag] == 1)   // User clicks Close Button
     {
-        // Restore the original image.
-       // [self restoreDCM:self.originalPix];
         
     }
 }
